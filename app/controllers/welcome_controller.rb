aa2da771e9b61ad944c9a658f97d6313db4393c0
zhu <zhu@zhu.com>
2011-11-11 23:28:25 +0800

    Add the demo project

---
+++ app/controllers/welcome_controller.rb
@@ -0,0 +1,10 @@
+class WelcomeController < ApplicationController
+	def hello
+	end
+
+	def goobye
+	end
+
+	def index
+	end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Demo::Application.config.secret_token = '86efcd6c3c2d168597eb77653ac3d6ad2fe4cebf127d1421f653dde9c7c17c90359e173ddd7b857daaef2896663b47d692d05e75dd196119410f2c104787a638'
